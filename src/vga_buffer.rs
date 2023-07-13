3b4babac727c3567449ab2332114f0ba79786f89
SparkySimp <info@yigitovski.com>
2023-07-14 00:12:09 +0300

    Added VGA module (WIP)

---
+++ src/vga_buffer.rs
@@ -0,0 +1,105 @@
+use spin::Mutex;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    LightGray = 0x7,
+    DarkGray = 0x8,
+    LightBlue = 0x9,
+    LightGreen = 0xA,
+    LightCyan = 0xB,
+    LightRed = 0xC,
+    Pink = 0xD,
+    Yellow = 0xE,
+    White = 0xF,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUF_HEIGHT: usize = 25;
+const BUF_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+        chars: [[ScreenChar; BUF_WIDTH]; BUF_HEIGHT],
+}
+
+pub struct Writer {
+        column_position: usize,
+        color_code: ColorCode,
+        buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUF_WIDTH {
+                    self.new_line();
+                }
+                let row = BUF_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row, col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+         };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+lazy_static! {
+        pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+                    column_position: 0,
+                    color_code: ColorCode::new(Color::Yellow, Color::Black),
+                    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+        });
+}
+
+
