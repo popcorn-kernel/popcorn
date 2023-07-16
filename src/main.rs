3c9b6c69203b19ffff027dfcd7d9aea0cc1f5ea6
Techiesplash <superpoweredvinny@gmail.com>
2023-07-15 21:46:58 -0700

    Quick fixes.

--- src/main.rs
+++ src/main.rs
@@ -24,39 +24,6 @@ mod serial;
 mod misc;
 
 pub fn init() {
-    // Set blink text mode to ON
-/*
-; Read I/O Address 0x03DA to reset index/data flip-flop
-	mov dx, 0x03DA
-	in al, dx
-	; Write index 0x30 to 0x03C0 to set register index to 0x30
-	mov dx, 0x03C0
-	mov al, 0x30
-	out dx, al
-	; Read from 0x03C1 to get register contents
-	inc dx
-	in al, dx
-	; Set Bit 3 to enable Blink
-	or al, 0x08
-	; Write to 0x03C0 to update register with changed value
-	dec dx
-	out dx, al
- */
-unsafe {
-        asm!("
-        mov dx, 0x03DA
-        in al, dx
-        mov dx, 0x03C0
-        mov al, 0x30
-        out dx, al
-        inc dx
-        in al, dx
-        or al, 0x08
-        dec dx
-        out dx, al
-        ");
-    }
-
     interrupts::init_interrupts();
 }
 
