e819fc21f24fd331c3dd2b545c2210b18af35b09
electron271 <66094410+electron271@users.noreply.github.com>
2023-10-23 14:48:20 -0500

    Revert "add some nicer colorisation to `compress-diskimg.sh` (#64)" (#68)
    This reverts commit 15c1a6f0a1040764c6b360e6ff067147039eebb3.

--- compress-diskimg.sh
+++ compress-diskimg.sh
@@ -2,57 +2,53 @@
 # compresses the popcorn disk image with zstd
 # using compression level 19
 
-# colors
-declare -A COLORS;
-
-COLORS[error]='\e[1;31m';
-COLORS[info]='\e[1m';
-COLORS[verbose]='\e[1;36m';
-COLORS[reset]='\e[0m';
-
 # check if `zstd` is present
 if ! command -v zstd; then
-	echo -e "[${COLORS[error]}ERROR${COLORS[reset]}] command \"zstd\" is not present" 1>&2;
+	echo -e "\e[1;31merror:\e[0m command \"zstd\" is not present" 1>&2;
 	exit 1;
 fi
 
 # check if `cargo` is present
 if ! command -v cargo; then
-	echo -e "[${COLORS[error]}ERROR${COLORS[reset]}] command \"cargo\" is not present" 1>&2;
+	echo -e "\e[1;31merror:\e[0m command \"cargo\" is not present" 1>&2;
 	exit 2;
 fi
 
 # check if `cargo bootimage` is present
 if ! command -v bootimage; then
-	echo -e "[${COLORS[error]}ERROR${COLORS[reset]}] cargo subcommand \"bootimage\" is not present" 1>&2;
+	echo -e "\e[1;31merror:\e[0m cargo subcommand \"bootimage\" is not present" 1>&2;
 	exit 3;
 fi
 
 # if all checks have passed, start building the kernel image
 # display the logo header first
-echo -e "${COLORS[info]}[INFO]${COLORS[reset]} building disk image...";
-echo -e "${COLORS[info]}[${COLORS[verbose]}EXEC${COLORS[reset]}${COLORS[info]}]${COLORS[reset]} cargo -v bootimage -v";
+echo "compress-diskimg.sh - Compresses a bootable popcorn disk image";
+echo "Copyright (c) 2023 The popcorn Project. See LICENSE for more information";
+echo "";
+
+echo -e "\e[1m[INFO]\e[0m building disk image...";
+echo -e "\e[1m[\e[1;36mEXEC\e[0m\e[1m]\e[0m cargo -v bootimage -v";
 # build the bootimage with verbose output
-exec cargo -v bootimage -v;
+cargo -v bootimage -v;
 
 if [ $? != 0 ]; then
-	echo -e "[${COLORS[error]}ERROR${COLORS[reset]}] \"cargo bootimage\" has failed." 2>&1;
+	echo -e "\e[1;31merror:\e[0m \"cargo bootimage\" has failed." 2>&1;
 	exit $(($? + 10));
 fi
 
 echo "";
-echo -e "${COLORS[info]}[INFO]${COLORS[reset]} compressing disk image...";
+echo -e "\e[1m[INFO]\e[0m compressing disk image...";
 # compress the disk image, with verbose output
-echo -e "${COLORS[info]}[${COLORS[verbose]}EXEC${COLORS[reset]}${COLORS[info]}]${COLORS[reset]} yes | zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin";
-exec yes | zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin;
+echo -e "\e[1m[\e[1;36mEXEC\e[0m\e[1m]\e[0m yes | zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin";
+yes | zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin;
 if [ $? != 0 ]; then
-	echo -e "[${COLORS[error]}ERROR${COLORS[reset]}] \"zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin\" failed" 2>&1;
+	echo -e "\e[1;31merror:\e[0m \"zstd -vz19o target/x86_64-arch/debug/bootimage-popcorn.bin.zst target/x86_64-arch/debug/bootimage-popcorn.bin\" failed" 2>&1;
 	exit $(($? + 20));
 fi
 
 # the disk image has been compressed. exit is safe
-echo -e "${COLORS[info]}[INFO]${COLORS[reset]} Compressed disk image successfully, written to \"target/x86_64-arch/debug/bootimage-popcorn.bin.zst\"";
+echo -e "\e[1m[INFO]\e[0m Compressed disk image successfully, written to \"target/x86_64-arch/debug/bootimage-popcorn.bin.zst\"";
 
 # i mean those who work on popcorn must be nocturnal people so...
-echo -e "${COLORS[info]}[INFO]${COLORS[reset]} popcorn project wishes you a nice day!";
+echo -e "\e[1m[INFO]\e[0m popcorn project wishes you a nice day!";
 exit 0;
